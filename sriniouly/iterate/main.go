5cefd4af7c887b66c4e150b5bb0163c09e41aa59
Ath1thyan <athitasks@gmail.com>
2025-12-18 19:51:02 +0530

    code updated

--- sriniouly/iterate/main.go
+++ sriniouly/iterate/main.go
@@ -6,24 +6,35 @@ import (
 )
 
 func main() {
-	fmt.Println("Saturday")
+	// fmt.Println("Saturday")
 	// fmt.Println(time.Now().Weekday())
 	// fmt.Println(time.Saturday - time.Now().Weekday())
 	fmt.Println(time.Now())
 
-	today := time.Now().Weekday()
-	switch time.Saturday {
-	case today + 0:
-		fmt.Println("Today")
-	case today + 1:
-		fmt.Println("Tomorrow")
-	case today + 2:
-		fmt.Println("In two days")
-	case today + 3:
-		fmt.Println("In three days")
-	case today + 4:
-		fmt.Println("In four days")
+	// today := time.Now().Weekday()
+	// switch time.Saturday {
+	// case today + 0:
+	// 	fmt.Println("Today")
+	// case today + 1:
+	// 	fmt.Println("Tomorrow")
+	// case today + 2:
+	// 	fmt.Println("In two days")
+	// case today + 3:
+	// 	fmt.Println("In three days")
+	// case today + 4:
+	// 	fmt.Println("In four days")
+	// default:
+	// 	fmt.Println("Too far away")
+	// }
+
+	t := time.Now()
+
+	switch {
+	case t.Hour() < 12:
+		fmt.Println("Good Morning")
+	case t.Hour() < 18:
+		fmt.Println("Good Afternoon")
 	default:
-		fmt.Println("Too far away")
+		fmt.Println("Good Evening")
 	}
 }
